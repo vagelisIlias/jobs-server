5516f1e01a534f5fe2919837cbfaa25c5ef4494e
vagelisIlias <evangelos.ilias87@gmail.com>
2025-09-06 00:40:05 +0300

    Added types on trait

--- app/ApiResponseTrait/ApiResponseTrait.php
+++ app/ApiResponseTrait/ApiResponseTrait.php
@@ -6,15 +6,15 @@ use Illuminate\Http\JsonResponse;
 
 trait ApiResponseTrait
 {
-    public function sendResponse($data, $message, int $code = 200): JsonResponse
+    public function sendResponse(mixed $data, string $message, int $code = 200): JsonResponse
     {
-    return response()->json([
-            'data' => $data,
-            'message' => $message
-    ], $code);
+        return response()->json([
+                'data' => $data,
+                'message' => $message
+        ], $code);
     }
 
-    public function sendError($message, $errors = [], int $code = 400): JsonResponse
+    public function sendError(string $message, $errors = [], int $code = 400): JsonResponse
     {
         return response()->json([
             'message' => $message,
